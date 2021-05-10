88441a36966a3bc9bacaf5d26471562169206f74
AnaAngel0831 <83310040+AnaAngel0831@users.noreply.github.com>
2021-05-10 22:26:24 +0300

    Add files via upload

---
+++ web.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BoardController;
+use App\Http\Controllers\TaskController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('login');
+});
+
+Route::match(['get', 'post'], '/login', [AuthController::class, 'login'])->name('login');
+Route::match(['get', 'post'], '/register', [AuthController::class, 'register'])->name('register');
+Route::get('/verify-email', [AuthController::class, 'verifyNotice'])->middleware('auth')->name('verification.notice');
+Route::get('verify-email/{id}/{hash}', [AuthController::class, 'verifyEmail'])->middleware(['auth', 'signed'])->name('verification.verify');
+Route::post('resend-verify-email', [AuthController::class, 'resendVerifyEmail'])->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+Route::match(['get', 'post'], '/forgot-password', [AuthController::class, 'forgotPassword'])->name('password.email');
+Route::match(['get', 'post'], '/reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::middleware(['admin'])->group(function () {
+        Route::get('/users', [AdminController::class, 'users'])->name('users.all');
+      //  Route::match(['get', 'post'], '/users/{id}', [AdminController::class, 'update'])->name('users.update');
+      //  Route::match(['get', 'post'], '/users/{id}', [AdminController::class, 'delete'])->name('users.delete');
+        Route::post('/users/{id}', [\App\Http\Controllers\AdminController::class, 'edit'])->name('UserEdit');
+
+    });
+    Route::get('/boards', [BoardController::class, 'boards'])->name('boards.all');
+    Route::get('/boards/{id}', [AdminController::class, 'update'])->name('boards.update');
+    Route::delete('/boards/{id}', [AdminController::class, 'delete'])->name('boards.delete');
+
+    Route::get('/tasks', [TaskController::class, 'tasks'])->name('tasks.all');
+    Route::get('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::get('/tasks/{id}', [TaskController::class, 'delete'])->name('tasks.delete');
+
+});
